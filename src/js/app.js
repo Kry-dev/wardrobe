b6bc2a1ad3e3672e48357641ac452ea91ee6c6e4
kry-dev <yaroslavkryvda@gmail.com>
2018-06-08 02:16:45 +0300

    add sidebar/ overlay/ dropdown menu

--- src/js/app.js
+++ src/js/app.js
@@ -5,5 +5,11 @@ $(document).ready(function () {
     $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
+        $('#overlay').fadeToggle( "slow", "swing" );
+    });
+
+    $('#overlay').click(function() {
+        $('#overlay').fadeOut('slow');
+        $('#wrapper').toggleClass('toggled');
     });
 });
