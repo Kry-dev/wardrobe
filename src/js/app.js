d00b40ec8414821d76f87e87f8674cc04da0223a
kry-dev <yaroslavkryvda@gmail.com>
2018-06-11 13:50:56 +0300

    add filter dropdawn

--- src/js/app.js
+++ src/js/app.js
@@ -12,5 +12,24 @@ $(document).ready(function () {
         $('#overlay').fadeOut('slow');
         $('#wrapper').toggleClass('toggled');
     });
+    $('.dropdown-menu').on('click', function(event) {
+        event.stopPropagation();
+    });
+
+    // $('.selectpicker').selectpicker({
+    //     container: 'body';
+    // });
+
+    $('body').on('click', function(event) {
+        var target = $(event.target);
+        if (target.parents('.bootstrap-select').length) {
+            event.stopPropagation();
+            $('.bootstrap-select.open').removeClass('open');
+        }
+    });
+    $(".dropdown-menu__wrap > .close").on('click' , function () {
+        console.log('click');
+        $(this).closest('.dropdown-menu').dropdown("toggle");
+    })
 
 });
